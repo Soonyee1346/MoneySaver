26653853f3a17d357e8e97d12095403dedf45e69
Soonyee1346 <92239495+Soonyee1346@users.noreply.github.com>
2024-03-22 13:26:03 +1100

    refresh

--- js/script.js
+++ js/script.js
@@ -4,5 +4,4 @@ function navBar() {
 
 function addIngredient() {
     $("#invisible").after('<input class="form Input" placeholder="Ingredient"/><div id="invisibleIng"></div>');
-    preventDefault();
 };
