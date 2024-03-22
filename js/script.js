f728017d612d78e17b159a6c804d67406c06902f
Soonyee1346 <92239495+Soonyee1346@users.noreply.github.com>
2024-03-22 15:14:10 +1100

    Testing nav bar locations

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-function navBar() {
-    $("#nav-placeholder").load("nav.html");
+function navBar(dir) {
+    if(dir == null){
+        $("#nav-placeholder").load("nav.html");
+    }
+    else {
+        dir = dir + "nav.html";
+        $("#nav-placeholder").load(dir);
+    }
 };
 
 function addIngredient() {
